f5ffc88178d79b82301f9f42c62a27e580e0ba1b
Your Name <rasool.expert.freelancer@gmail.com>
2024-12-09 03:30:31 +0500

    update the code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ interface ButtonProps {
   text: string; 
 }
 const Button = (props:ButtonProps) => {
-  return <div className="py-[8px] px-[22px] bg-[#000000]  rounded-[400px] text-white">{props.text}</div>;
+  return <div className=" py-[8px] px-[22px] bg-[#000000]  rounded-[400px] text-white">{props.text}</div>;
 };
 
 export default Button;
